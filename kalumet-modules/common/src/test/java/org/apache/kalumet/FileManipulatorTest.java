af6d4c69dcd22a7cefdf8ac1fcc7d922c30644dc
Olivier Lamy <olamy@apache.org>
2012-07-09 19:48:48 +0000

    make possible to run tests when offline
    git-svn-id: https://svn.apache.org/repos/asf/incubator/kalumet/trunk@1359369 13f79535-47bb-0310-9956-ffa450edef68

--- kalumet-modules/common/src/test/java/org/apache/kalumet/FileManipulatorTest.java
+++ kalumet-modules/common/src/test/java/org/apache/kalumet/FileManipulatorTest.java
@@ -18,8 +18,20 @@
  */
 package org.apache.kalumet;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -29,51 +41,97 @@ import static org.junit.Assert.assertEquals;
 public class FileManipulatorTest
 {
 
-  private FileManipulator fileManipulator;
+    private FileManipulator fileManipulator;
 
-  @Before
-  public void setUp()
-    throws Exception
-  {
-    fileManipulator = new FileManipulator();
-  }
+    private Tomcat tomcat;
 
-  @Test
-  public void testProtocolPrefix()
-    throws Exception
-  {
-    String uri = "http://uri";
-    assertEquals( true, FileManipulator.protocolExists( uri ) );
-    uri = "uri";
-    assertEquals( false, FileManipulator.protocolExists( uri ) );
-    uri = "zip:file:/test";
-    assertEquals( true, FileManipulator.protocolExists( uri ) );
-  }
+    private int port;
 
-  @Test
-  public void testVFSFormatting()
-    throws Exception
-  {
-    String uri = FileManipulator.format( "http://uri/archive.tar.gz" ) + "!/lib";
-    assertEquals( "tgz:http://uri/archive.tar.gz!/lib", uri );
-  }
+    @Before
+    public void setUp()
+        throws Exception
+    {
+        fileManipulator = new FileManipulator();
+        tomcat = new Tomcat();
+        tomcat.setBaseDir( System.getProperty( "java.io.tmpdir" ) );
+        tomcat.setPort( 0 );
 
-  @Test
-  public void testContentCompare()
-    throws Exception
-  {
-    assertEquals( true,
-                  fileManipulator.contentEquals( "https://svn.apache.org/repos/asf/incubator/kalumet/trunk/LICENSE",
-                                                 "https://svn.apache.org/repos/asf/incubator/kalumet/trunk/LICENSE" ) );
-  }
+        Context context = tomcat.addContext( "", System.getProperty( "java.io.tmpdir" ) );
 
-  @Test
-  public void testExists()
-    throws Exception
-  {
-    assertEquals( true, fileManipulator.exists( "https://svn.apache.org/repos/asf/incubator/kalumet/trunk/LICENSE" ) );
-    assertEquals( false,
-                  fileManipulator.exists( "https://svn.apache.org/repos/asf/incubator/kalumet/trunk/NOT_FOUND" ) );
-  }
+        Tomcat.addServlet( context, "foo", new SimpleGetServlet() );
+        context.addServletMapping( "/repos/*", "foo" );
+
+        tomcat.start();
+
+        this.port = tomcat.getConnector().getLocalPort();
+
+    }
+
+    private static class SimpleGetServlet
+        extends HttpServlet
+    {
+        @Override
+        protected void doGet( HttpServletRequest req, HttpServletResponse resp )
+            throws ServletException, IOException
+        {
+            if ( req.getPathInfo().endsWith( "LICENSE" ) )
+            {
+                String basedir = System.getProperty( "basedir" );
+                File f = new File( basedir, "src/test/resources/LICENSE" );
+                IOUtils.copy( new FileInputStream( f ), resp.getOutputStream() );
+                return;
+            }
+
+            resp.setStatus( 404 );
+        }
+    }
+
+    @After
+    public void shutdown()
+        throws Exception
+    {
+
+        tomcat.stop();
+
+    }
+
+    @Test
+    public void testProtocolPrefix()
+        throws Exception
+    {
+        String uri = "http://uri";
+        assertEquals( true, FileManipulator.protocolExists( uri ) );
+        uri = "uri";
+        assertEquals( false, FileManipulator.protocolExists( uri ) );
+        uri = "zip:file:/test";
+        assertEquals( true, FileManipulator.protocolExists( uri ) );
+    }
+
+    @Test
+    public void testVFSFormatting()
+        throws Exception
+    {
+        String uri = FileManipulator.format( "http://uri/archive.tar.gz" ) + "!/lib";
+        assertEquals( "tgz:http://uri/archive.tar.gz!/lib", uri );
+    }
+
+    @Test
+    public void testContentCompare()
+        throws Exception
+    {
+        assertEquals( true, fileManipulator.contentEquals(
+            "http://localhost:" + port + "/repos/asf/incubator/kalumet/trunk/LICENSE",
+            "http://localhost:" + port + "/repos/asf/incubator/kalumet/trunk/LICENSE" ) );
+    }
+
+    @Test
+    public void testExists()
+        throws Exception
+    {
+        assertEquals( true, fileManipulator.exists(
+            "http://localhost:" + port + "/repos/asf/incubator/kalumet/trunk/LICENSE" ) );
+        assertEquals( false, fileManipulator.exists(
+            "http://localhost:" + port + "/repos/asf/incubator/kalumet/trunk/NOT_FOUND" ) );
+    }
 
 }
